99c5ae30ae94f9c93a0aa5d94853bd3bafe1be23
AnggaMaulana6 <anggaefendi2006@gmail.com>
2023-11-14 20:14:01 +0700

    CRUD Dasar Laeavel

---
+++ app/Http/Controllers/Register.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class Register extends Controller
+{
+    public function index()
+    {
+        return view('register');
+    }
+
+    public function store(Request $request)
+    {
+        $validation = $request->validate([
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255',
+        ]);
+        $validation['password'] = Hash::make($validation['password']);
+
+        User::create($validation);
+        return redirect('/login')->with('sucess','Regesiter Berhasil! Silakan Login');
+    }
+}
